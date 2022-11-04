5978367247a6aff8a03e30c58503ae03a99e76f0
tamir3232 <tamirrasydi@gmail.com>
2022-11-05 00:15:06 +0700

    menambah database tracker spend dan income

--- index.js
+++ index.js
@@ -6,4 +6,21 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT
 
-console.log(port)
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+    res.send({
+        Message: "SELAMAT DATANG DI EXPENSE TRACKER"
+    })
+})
+app.use((err, req, res, next) => {
+    return res.status(err.code || 500).json({
+        message: err.message || 'internal server eror'
+    })
+})
+
+app.listen(port, () => {
+    console.log(`SERVER RUNING ATT PORT ${port}`)
+})
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
+npm int -y
+npm install prisma --save-dev
+npx prisma init
+npx prisma migrate dev --name init
+npm install @prisma/client
 npm install dotenv
 npm install express
- npm install cors   
+npm install cors   
